9c1e42a30f9bc346c6ab25f7bb784edaea4568fb
Jieun Kim <jisilver.kim@gmail.com>
2020-01-23 00:01:03 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -16,4 +16,44 @@
           delay: 5000,
           },
       });
+
+    // 숫자 카운팅
+    $('.counter_value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      
+      $({ countNum: $this.text()}).animate({
+        countNum: countTo
+      },
+
+      {
+        duration: 3000,
+        easing:'linear',
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+          //alert('finished');
+        }
+      });  
+
+});
+      //post 이미지 슬라이드
+      var swiper = new Swiper('.swiper-container2', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+      });
 })(jQuery);  
