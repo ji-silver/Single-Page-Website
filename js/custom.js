99d4e80f237db11ef8ddbf58980093f5aaa8f0d7
Jieun Kim <jisilver.kim@gmail.com>
2020-01-25 00:05:00 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -58,15 +58,54 @@
           delay: 5000,
           },
         breakpoints: {
-          // when window width is >= 0px
+          // when window width is > 0px
           0: {
             slidesPerView: 1,
-            spaceBetween: 30
+            spaceBetween: 20
           },
-          // when window width is >= 601px
+          // when window width is > 600px
           601: {
             slidesPerView: 3,
+            spaceBetween: 30
+          }
+        }
+        
+      });
+
+      var swiper = new Swiper('.swiper-container3', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+          // when window width is > 0px
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          // when window width is > 320px
+          321: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          // when window width is > 768px
+          769: {
+            slidesPerView: 3,
             spaceBetween: 40
+          },
+          // when window width is > 960px
+          961: {
+            slidesPerView: 4,
+            spaceBetween: 50
           }
         }
         
