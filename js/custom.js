91239b1a989a06f77aae4639ae953acfa24c9198
Jieun Kim <jisilver.kim@gmail.com>
2020-01-28 00:00:19 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -111,4 +111,21 @@
         }
         
       });
+
+      // Header scroll시 class 추가
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
+      } else {
+        $('#header').removeClass('header-scrolled');
+      }
+    });
+  
+    if ($(window).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    }
+
+    
+
+    
 })(jQuery);  
